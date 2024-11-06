ebe1c067b345971fe6ffaebb2d9c6a766f38d53e
Jin-tonix <jinheemok815@gmail.com>
2024-11-06 15:34:17 +0900

    Updated project with recent changes

---
+++ build/precache-manifest.a8fdb09f131dcfeb67eb9f556ae04fd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7893176848b1980bad256f355fd5adff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cfa5db6747ec950ecd6",
+    "url": "/static/css/main.13456c6e.chunk.css"
+  },
+  {
+    "revision": "c0d036189c249951b206",
+    "url": "/static/js/2.d74ab52d.chunk.js"
+  },
+  {
+    "revision": "db75c9ea4f6d1433dc80ed0353c58b54",
+    "url": "/static/js/2.d74ab52d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e91d07666192c42b25",
+    "url": "/static/js/3.dfa55c1e.chunk.js"
+  },
+  {
+    "revision": "9cfa5db6747ec950ecd6",
+    "url": "/static/js/main.e451404f.chunk.js"
+  },
+  {
+    "revision": "479bf07705a9998233bc",
+    "url": "/static/js/runtime-main.81e5b7eb.js"
+  }
+]);
