0540926fca0274ec82a0addfe70bc2deed61e59d
Jin-tonix <jinheemok815@gmail.com>
2024-11-07 17:18:35 +0900

    Deploying to GitHub Pages

---
+++ precache-manifest.bf0cc04f18375991f9fcfee213580e5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09614dd12de951e888ead0b33f416397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbf261047df3a4798f60",
+    "url": "/static/css/main.f4493b98.chunk.css"
+  },
+  {
+    "revision": "80824770e3fcb0a3a1b9",
+    "url": "/static/js/2.44ab3fd8.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/static/js/2.44ab3fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01ea54470a6d3247611",
+    "url": "/static/js/3.94666343.chunk.js"
+  },
+  {
+    "revision": "fbf261047df3a4798f60",
+    "url": "/static/js/main.1194ab1b.chunk.js"
+  },
+  {
+    "revision": "a84460836a9db0a23981",
+    "url": "/static/js/runtime-main.95c4052e.js"
+  }
+]);
