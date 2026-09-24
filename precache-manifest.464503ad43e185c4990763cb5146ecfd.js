92e04fbdf8fa2f4065070525a4ed8414e002da20
Jin-tonix <jinheemok815@gmail.com>
2026-09-24 13:12:31 +0900

    Deploy About centering

---
+++ precache-manifest.464503ad43e185c4990763cb5146ecfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd95463c8e04efc8f94de608e2ad8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5fbadfab876e2fa389d4",
+    "url": "/static/css/main.fee3dd61.chunk.css"
+  },
+  {
+    "revision": "ade9b53eee63225e7058",
+    "url": "/static/js/2.9a77417c.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/static/js/2.9a77417c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec0c2f78104080d4bcc",
+    "url": "/static/js/3.7125a747.chunk.js"
+  },
+  {
+    "revision": "5fbadfab876e2fa389d4",
+    "url": "/static/js/main.33ded39f.chunk.js"
+  },
+  {
+    "revision": "bbe725c65674028a1614",
+    "url": "/static/js/runtime-main.262b3f3e.js"
+  }
+]);
