f43a02a6be05d7b48802689e95dea148c25b3886
Jin-tonix <jinheemok815@gmail.com>
2026-09-24 13:07:39 +0900

    Deploy About copy

---
+++ precache-manifest.7a18307360c46961ff1ed96d8f0905d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edc6228d581cb9c35d11134d0b0089f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3a6cb1d2017b02c4a5d",
+    "url": "/static/css/main.fee3dd61.chunk.css"
+  },
+  {
+    "revision": "ade9b53eee63225e7058",
+    "url": "/static/js/2.9a77417c.chunk.js"
+  },
+  {
+    "revision": "09dfcff8991931712817539b48cc9b6c",
+    "url": "/static/js/2.9a77417c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ec0c2f78104080d4bcc",
+    "url": "/static/js/3.7125a747.chunk.js"
+  },
+  {
+    "revision": "c3a6cb1d2017b02c4a5d",
+    "url": "/static/js/main.c440a266.chunk.js"
+  },
+  {
+    "revision": "bbe725c65674028a1614",
+    "url": "/static/js/runtime-main.262b3f3e.js"
+  }
+]);
