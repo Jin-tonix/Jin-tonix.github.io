421e2cf9d852f14b19ac35ada47bd5a44048477b
Jin-tonix <jinheemok815@gmail.com>
2026-09-25 13:30:18 +0900

    Deploy About rewrite

--- precache-manifest.1134f8c8fbf9fb9082bafc735ce48078.js
+++ precache-manifest.1134f8c8fbf9fb9082bafc735ce48078.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0fd29ff04dcd0c7c35a43d4409522092",
+    "revision": "088d30434062733a681872b706584fcd",
     "url": "/index.html"
   },
   {
-    "revision": "681e2f07eace20e82580",
+    "revision": "beeadcfbdb0fd7996ebb",
     "url": "/static/css/main.314543d8.chunk.css"
   },
   {
-    "revision": "68f56454fabeeaa0b8dd",
-    "url": "/static/js/2.f46e4001.chunk.js"
+    "revision": "466ac533246b42b059fb",
+    "url": "/static/js/2.e41e6a63.chunk.js"
   },
   {
     "revision": "09dfcff8991931712817539b48cc9b6c",
-    "url": "/static/js/2.f46e4001.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.e41e6a63.chunk.js.LICENSE.txt"
   },
   {
     "revision": "06c6dbca5df590f78e59",
     "url": "/static/js/3.842cb038.chunk.js"
   },
   {
-    "revision": "681e2f07eace20e82580",
-    "url": "/static/js/main.c5a249fa.chunk.js"
+    "revision": "beeadcfbdb0fd7996ebb",
+    "url": "/static/js/main.e5f0f702.chunk.js"
   },
   {
     "revision": "3c4b25d18c9a142b3430",
