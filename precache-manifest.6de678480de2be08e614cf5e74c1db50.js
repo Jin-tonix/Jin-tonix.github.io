a658144a5b11be1a0ac342b7a8821dc1f1cddff9
Jin-tonix <jinheemok815@gmail.com>
2026-09-25 14:12:30 +0900

    Deploy About deck copy

--- precache-manifest.6de678480de2be08e614cf5e74c1db50.js
+++ precache-manifest.6de678480de2be08e614cf5e74c1db50.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea30ba41b70c7226ee7b7cf2d3443c46",
+    "revision": "c408ec7b99524f8d6d0b0fae5fd48246",
     "url": "/index.html"
   },
   {
-    "revision": "22b9fb5f701df3aceb86",
+    "revision": "d9282a94a2f42299a87f",
     "url": "/static/css/main.314543d8.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/3.842cb038.chunk.js"
   },
   {
-    "revision": "22b9fb5f701df3aceb86",
-    "url": "/static/js/main.5e6837e3.chunk.js"
+    "revision": "d9282a94a2f42299a87f",
+    "url": "/static/js/main.1ac23a49.chunk.js"
   },
   {
     "revision": "3c4b25d18c9a142b3430",
